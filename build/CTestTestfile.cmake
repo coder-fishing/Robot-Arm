57bd813cb1254d2fe63d6ddb9641d0dad89c7eb9
quoctrieu <quoctrieu@NIBUbuntu.myguest.virtualbox.org>
2025-12-19 23:25:07 +0700

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,21 @@
+# CMake generated Testfile for 
+# Source directory: /home/quoctrieu/ur5_ws/src
+# Build directory: /home/quoctrieu/ur5_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("universal_robot/universal_robots")
+subdirs("universal_robot/ur10_moveit_config")
+subdirs("universal_robot/ur10e_moveit_config")
+subdirs("universal_robot/ur16e_moveit_config")
+subdirs("universal_robot/ur3_moveit_config")
+subdirs("universal_robot/ur3e_moveit_config")
+subdirs("ur5_control")
+subdirs("ur5_gazebo")
+subdirs("universal_robot/ur5_moveit_config")
+subdirs("ur5_objects")
+subdirs("universal_robot/ur5e_moveit_config")
+subdirs("universal_robot/ur_description")
+subdirs("universal_robot/ur_gazebo")
+subdirs("universal_robot/ur_kinematics")
